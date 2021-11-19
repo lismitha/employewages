466a1db4b7ce1b990f6a003a8253801ef033c88a
lismitha <lismithaspatel@gmail>
2021-11-19 13:54:01 +0530

    [lismitha]adding uc2 file

--- employewages1/employe.cs
+++ employewages1/employe.cs
@@ -7,23 +7,30 @@ using System.Threading.Tasks;
 namespace employewages1
 {
     internal class employe
-    {  
-            static void Main(string[] args)
+    {
+        static void Main(string[] args)
+        {
+            //constants
+            int IS_FILL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            //Variable
+            int empHrs = 0;
+            int empwages = 0;
+            Random random = new Random();
+            //computation
+            int empcheck = random.Next(0, 2);
+            if (empcheck == IS_FILL_TIME)
             {
-                //constants
-                int IS_FILL_TIME = 1;
-                Random random = new Random();
-                //computation
-                int empcheck = random.Next(0, 2);
-                if (empcheck == IS_FILL_TIME)
-                {
-                    Console.WriteLine("Employ is present");
-                }
-                else
-                {
-                    Console.WriteLine("Employe is absent");
-                }
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empwages = empwages * EMP_RATE_PER_HOUR;
 
+            Console.WriteLine("emp wages: + emp wages");
+       
             }
         }
     }
