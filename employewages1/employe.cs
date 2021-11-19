2d018d7c86cf96b2b0ff4c83fc32bdd707f69844
lismitha <lismithaspatel@gmail>
2021-11-19 12:38:21 +0530

    [lismitha]adding uc1branch

---
+++ employewages1/employe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employewages1
+{
+    internal class employe
+    {  
+            static void Main(string[] args)
+            {
+                //constants
+                int IS_FILL_TIME = 1;
+                Random random = new Random();
+                //computation
+                int empcheck = random.Next(0, 2);
+                if (empcheck == IS_FILL_TIME)
+                {
+                    Console.WriteLine("Employ is present");
+                }
+                else
+                {
+                    Console.WriteLine("Employe is absent");
+                }
+
+            }
+        }
+    }
