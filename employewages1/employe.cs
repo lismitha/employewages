857e76be722e24382d18193b5a9356d9fa22ddb6
lismitha <lismithaspatel@gmail>
2021-11-30 23:43:08 +0530

    [lismitha]adding uc8employewage

--- employewages1/employe.cs
+++ employewages1/employe.cs
@@ -10,54 +10,46 @@ namespace EmpWage
     {
         public const int is_Full_Time = 1;
         public const int is_Part_Time = 2;
-        public const int RatePerHrs = 20;
-        public const int Max_Working_Days = 20;
-        public const int Max_Working_Hrs = 100;
 
-        public static int computeEmpwage()
+        public static int computeEmpwage(string company,int empRatePerHr,int numOfWorkinDays,int maxHrPerMonth)
         {
-            int empWage = 0;
             int empHrs = 0;
-            int totalEmpWage = 0;
             int totalEmpHrs = 0;
-            int totalDays = 1;
-            string typeOfEmployee = "";
+            int totalWorkingDays = 1;
 
             Random employee = new Random();
-            while (totalEmpHrs <= Max_Working_Hrs && totalDays <= Max_Working_Days)
+            while (totalEmpHrs <= maxHrPerMonth && totalWorkingDays <= numOfWorkinDays)
             {
-                int randomInput = employee.Next(0, 3);
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3); ;
 
-                switch (randomInput)
+                switch (empCheck)
                 {
                     case is_Full_Time:
                         empHrs = 8;
-                        typeOfEmployee = "Full Time";
                         break;
 
                     case is_Part_Time:
                         empHrs = 4;
-                        typeOfEmployee = "Part Time";
                         break;
 
                     default:
                         empHrs = 0;
-                        typeOfEmployee = "Absent";
                         break;
                 }
-                totalEmpHrs = totalEmpHrs + empHrs;
-                empWage = RatePerHrs * empHrs;
-                totalEmpWage = RatePerHrs * totalEmpHrs;
-                Console.WriteLine("{0}Emplyee of day: {1}wage is {2}", typeOfEmployee, totalDays, empWage);
-                totalEmpWage = totalEmpWage + empWage;
-                totalDays++;
+                     totalEmpHrs = totalEmpHrs += empHrs;
+                     Console.WriteLine("days#:" + totalWorkingDays + "  Employe Hours:" + empHrs);
             }
-            Console.WriteLine("Employee wage for {0} day is : {1}", Max_Working_Days, totalEmpWage);
-            return totalEmpWage;
+                     int TotalEmpWage = totalEmpHrs * empRatePerHr;
+               
+                     Console.WriteLine("Totall Employe wage per company:" + company + " is:" +TotalEmpWage);
+                     return TotalEmpWage;     
         }
-          static void Main(string[] args)
-        {
-            computeEmpwage();
-        }
+           static void Main(string[] args)
+           {
+                computeEmpwage("vedantu", 21,2,10);
+                computeEmpwage("byjus", 11, 5, 20);
+           }
     }
 }
